8eb5c9132304aa8499482203c848c201883e3214
barkstone <barkstone2@gmail.com>
2023-04-22 13:56:53 +0900

    feat: 물 정보 엔티티 추가

---
+++ app/src/main/java/com/note/coffee/data/entity/water/Water.kt
@@ -0,0 +1,11 @@
+package com.note.coffee.data.entity.water
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Water(
+    @PrimaryKey(autoGenerate = true) val id: Long = 0,
+    val name: String?,
+    val comment: String? = null
+)
